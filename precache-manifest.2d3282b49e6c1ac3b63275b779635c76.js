f1d06f78e12f9a61a2bbb4db1c15822ea53e2c68
han <hyochang84@gmail.com>
2020-09-07 22:14:13 +0900

    Updates

---
+++ precache-manifest.2d3282b49e6c1ac3b63275b779635c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b316b2f09b205b5fa9b66b4ff0175d6",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "c066265c560b8712c9fd",
+    "url": "/movie_app_2019/static/css/main.21bc3d3e.chunk.css"
+  },
+  {
+    "revision": "e3ee12e8d6e602c8dff7",
+    "url": "/movie_app_2019/static/js/2.ae81af6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.ae81af6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c066265c560b8712c9fd",
+    "url": "/movie_app_2019/static/js/main.0ff3a50e.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
